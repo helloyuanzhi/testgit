a0f06787e295cca23b9d5210b040823a5ff20119
helloyuanzhi <2446351171@qq.com>
2018-05-24 23:31:55 +0800

    222

--- Autotest/Test_Demo_Requests/test_coach_sin.py
+++ Autotest/Test_Demo_Requests/test_coach_sin.py
@@ -11,6 +11,7 @@ def coach_sin():
         api_name = '教练签到接口'
         print "宇豪测试git merge "
         print "111111"
+        print "222222"
         url = 'v1/coach_app/coachSigIn'
 
         params = {
