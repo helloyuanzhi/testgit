0e5c82dde5b4008646f5587faa541617290e98e9
yuhao <yuhao@leoao.com>
2018-08-16 18:14:52 +0800

    用户分群脚本上传

--- Test_Runner/debugtalk.py
+++ Test_Runner/debugtalk.py
@@ -2,8 +2,16 @@
 import random
 
 
-def get_account(num):
+def get_userid(num):
     account = []
     for i in range(0, num):
-        account.append({"coachId": "12%s" % i})
+        random_userid = random.randint(100,100000)
+        account.append({"user_id": random_userid})
+    return account
+
+def get_grouparry(num):
+    account = []
+    for i in range(0, num):
+        random_userid = random.randint(100,100000)
+        account.append({"coachId": random_userid})
     return account
